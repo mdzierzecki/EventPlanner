352659a457bcbef49c97b4d0b18b5ffc1433c6d1
mdzierzecki <mateusz@dzierzecki.com>
2019-12-18 23:22:11 +0100

    Some comments added

--- eventplanner/urls.py
+++ eventplanner/urls.py
@@ -23,7 +23,7 @@ from ckeditor_uploader import views
 urlpatterns = [
     path('', include('startpage.urls')),
     path('', include('events.urls')),
-    path('ckeditor/upload', views.upload, name='ckeditor_upload'),
-    path('ckeditor/browse', views.browse, name='ckeditor_browse'),
+    path('ckeditor/upload', views.upload, name='ckeditor_upload'),   # special path for upload
+    path('ckeditor/browse', views.browse, name='ckeditor_browse'),   # special path for browse, both connected to some CKEUploader glitch
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
