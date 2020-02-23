40a692335cbbb4c324a682e301ee1379d9413225
mdzierzecki <mateusz@dzierzecki.com>
2020-02-23 19:04:08 +0100

    Email to admin when somebody sign up

--- startpage/models.py
+++ startpage/models.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,3 +6,14 @@ from django.contrib.auth.models import User
 class eUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     from_where = models.TextField()
+
+    def save(self, force_insert=False, force_update=False):
+
+        send_mail(
+            'ZipEvent: Nowy użytkownik',
+            'Zarejestrowano nowego użytkownika: {}'.format(self.user),
+            'ZipEvent Team <no-reply@slickcode.pl>',
+            ['mateusz@dzierzecki.com'],
+            fail_silently=False,
+        )
+        super(eUser, self).save(force_insert, force_update)
